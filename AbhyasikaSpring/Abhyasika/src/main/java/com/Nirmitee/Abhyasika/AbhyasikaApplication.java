6f50465ae760a32f694ba18bd9140ef987d63c5f
unknown <shukrenishant@gmail.com>
2024-09-28 14:04:36 +0530

    spring init

---
+++ AbhyasikaSpring/Abhyasika/src/main/java/com/Nirmitee/Abhyasika/AbhyasikaApplication.java
@@ -0,0 +1,13 @@
+package com.Nirmitee.Abhyasika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhyasikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhyasikaApplication.class, args);
+	}
+
+}
---
+++ AbhyasikaSpring/Abhyasika/src/test/java/com/Nirmitee/Abhyasika/AbhyasikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nirmitee.Abhyasika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhyasikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
